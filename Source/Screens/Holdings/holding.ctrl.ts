acc1d4d282500b64eb4e19b0fe4089c326365c27
Ravi K <jarvicephp@gmail.com>
2024-07-09 11:16:54 +0530

    feat: add basic screens

---
+++ Source/Screens/Holdings/holding.ctrl.ts
@@ -0,0 +1,1 @@
+// All buiness logics here
---
+++ Source/Screens/Holdings/index.tsx
@@ -0,0 +1,15 @@
+// UI of holding listing
+import { View, Text } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const UserHoldings = (props: Props) => {
+  return (
+    <View>
+      <Text>UserHoldings</Text>
+    </View>
+  )
+}
+
+export default UserHoldings
